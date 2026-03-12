2dfb752fd6529d628b405d9a478af7a145137a27
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-12 14:48:16 +0700

    menambahkan logika pergeseran data insertion sort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -42,5 +42,11 @@ void insertionsort()
     {
         temp = arr[i];
         j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
     }
 }
