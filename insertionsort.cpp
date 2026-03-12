c926be8ff4461ba1bca47facc5e6e13c4ec8a4ea
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-12 14:49:56 +0700

    menampilkan langkah proses penyortiran

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -48,5 +48,13 @@ void insertionsort()
             arr[j + 1] = arr[j];
             j--;
         }
+        
+         arr[j + 1] = temp;
+
+        cout << "\nstep " << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
     }
 }
